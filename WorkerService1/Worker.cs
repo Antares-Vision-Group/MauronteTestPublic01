15943f1f669e8b3ed78346f1f24eee93fbc871d9
MauroneAV <126657108+MauroneAV@users.noreply.github.com>
2024-09-11 12:57:45 +0000

    Refactor Worker class to use a static logger message for worker running

--- WorkerService1/Worker.cs
+++ WorkerService1/Worker.cs
@@ -4,17 +4,40 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
 
+    private static Action<ILogger, DateTimeOffset, Exception> _workerRunning;
+
+    static Worker()
+    {
+        _workerRunning = LoggerMessage.Define<DateTimeOffset>(
+            LogLevel.Information,
+            new EventId(1, "Worker running"),
+            "Worker running at: {time}");
+    }
+
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// This method is called when the <see cref="IHostedService"/> starts. The implementation should return a task that represents the lifetime of the long
+    /// </summary>
+    /// <param name="stoppingToken"></param>
+    /// <returns></returns>
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            _workerRunning(_logger, DateTimeOffset.Now, null!);  
+            try
+            {
+                await Task.Delay(1000, stoppingToken);
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogInformation("Task was canceled");
+                break;
+            }
         }
     }
 }
